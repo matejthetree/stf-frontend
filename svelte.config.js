d90f2c22b09eba899902b71cfb05f74d696a3a44
matej <matejthetree@gmail.com>
2024-10-12 14:53:14 +0000

    Revert "port"
    This reverts commit 75a62b0ecdd39a4ec4bb560fd4de416687ed6a3a.

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		vite: {
-			server: {
-				host: '0.0.0.0',
-				port: 4173,
-			}
-		}
+		adapter: adapter()
 	}
 };
 export default config;
