71d1c499a27c3dead56c58e77cf5cbc814729ade
matej <matejthetree@gmail.com>
2024-10-13 19:54:49 +0000

    fix for qr manager

--- src/components/QRManager.svelte
+++ src/components/QRManager.svelte
@@ -28,7 +28,7 @@
 	}
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen ">
+<div class="flex flex-col items-center justify-center ">
 
 	{#if $qrCodeDataUrl}
 		<img src={$qrCodeDataUrl} alt="QR Code" class="w-48 mb-4" />
